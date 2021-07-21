5565a4fd227604ffd3a7bcffabc94dbcf40120ac
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-07-21 20:08:19 -0300

    WIP: Login form

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,31 +1,48 @@
 <template>
-     <v-container fluid fill-height>
-        <form>
-            <v-text-field
-                v-model="form.username"
-                label="Usuário"
-            >
-            </v-text-field>
-            <v-text-field
-                v-model="form.password"
-                label="Senha"
-            >
-            </v-text-field>
-
-            <v-btn
-            class="mr-4"
+  <v-card elevation="10">
+    <v-form v-model="valid">
+      <v-container fluid>
+        <v-text-field
+          v-model="form.username"
+          prepend-inner-icon="mdi-account"
+          :rules="usernameRules"
+          type="text"
+          label="Usuário"
+        ></v-text-field>
+        <v-text-field
+          v-model="form.password"
+          prepend-inner-icon="mdi-lock"
+          :rules="passwordRules"
+          type="password"
+          label="Senha"
+        ></v-text-field>
+        <v-card-actions>
+          <v-btn
+            color="#63FFAC"
+            x-large block
             @click="login"
-            >
+          >
             Entrar
-            </v-btn>
-        </form>    
-     </v-container>
-    
+          </v-btn>
+        </v-card-actions>
+       
+      </v-container>
+    </v-form>
+  </v-card>
+  
 </template>
 
 <script>
 export default {
     data: () => ({
+      loading: false,
+      valid: true,
+      usernameRules: [
+        f => !!f || 'Preencha o campo usuário'
+      ],
+      passwordRules: [
+         f => !!f || 'Preencha o campo senha'
+      ],
       form: {
         username: '',
         password: '',
@@ -39,15 +56,18 @@ export default {
         this.$axios.$post('/login', this.form)
         .then((response) => {
           this.user = response.data;
+          this.authenticated = true;
         }).catch((err) => {
           return err.message;
         }).finally(() => {
           setTimeout(() => {
-            this.authenticated = true;
+            if(this.authenticated) {
+              // Do some stuff
+            }
           }, 500);
         })
 
-      }
+      },
     }
 }
 </script>
