f8b955a9af3d7676c3fc09d812a1d635fee46e0e
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-07-21 23:23:49 -0300

    WIP

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,42 +1,56 @@
 <template>
-  <v-card elevation="10">
-    <v-form v-model="valid">
-      <v-container fluid>
-        <v-text-field
-          v-model="form.username"
-          prepend-inner-icon="mdi-account"
-          :rules="usernameRules"
-          type="text"
-          label="Usuário"
-        ></v-text-field>
-        <v-text-field
-          v-model="form.password"
-          prepend-inner-icon="mdi-lock"
-          :rules="passwordRules"
-          type="password"
-          label="Senha"
-        ></v-text-field>
-        <v-card-actions>
-          <v-btn
-            color="#63FFAC"
-            x-large block
-            @click="login"
-          >
-            Entrar
-          </v-btn>
-        </v-card-actions>
-       
-      </v-container>
-    </v-form>
-  </v-card>
-  
+  <v-row align="center">
+    <v-card 
+      elevation="12"
+    >
+      <v-form v-model="valid">
+        <v-container fluid>
+          <v-card-title>
+            Login
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="form.username"
+              prepend-inner-icon="mdi-account"
+              :rules="usernameRules"
+              type="text"
+              label="Usuário"
+            ></v-text-field>
+            <v-text-field
+              v-model="form.password"
+              prepend-inner-icon="mdi-lock"
+              :rules="passwordRules"
+              type="password"
+              label="Senha"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              class="ma-2"
+              color="#63FFAC"
+              x-large block
+              :loading="loading"
+              :disabled="loading"
+              @click="loader = 'loading'"
+              @submit.prevent="login"
+            >
+              <span>
+                Entrar
+              </span>
+            </v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-form>
+    </v-card>
+  </v-row>
 </template>
 
 <script>
 export default {
     data: () => ({
-      loading: false,
       valid: true,
+      loader: null,
+      loading: false,
       usernameRules: [
         f => !!f || 'Preencha o campo usuário'
       ],
@@ -48,26 +62,48 @@ export default {
         password: '',
       },
       authenticated: false,
-      user: {}
     }),
+
+    watch: {
+      loader () {
+        const l = this.loader;
+        this[l] = !this[l];
+        setTimeout(() => (
+          this[l] = false
+        ), 1500);
+        this.loader = null;
+      },
+    },
 
     methods: {
       login() {
         this.$axios.$post('/login', this.form)
         .then((response) => {
-          this.user = response.data;
+          sessionStorage.setItem('USER_LOGIN', response.data)
           this.authenticated = true;
         }).catch((err) => {
           return err.message;
         }).finally(() => {
           setTimeout(() => {
-            if(this.authenticated) {
-              // Do some stuff
+            this.authenticated = true;
+            if (this.authenticated) {
+              this.$router.push('/')
             }
-          }, 500);
+          }, 1500);
         })
 
       },
     }
 }
 </script>
+
+<style scoped>
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
